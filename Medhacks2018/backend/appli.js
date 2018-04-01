64c710d3b74b0628d3001bdf9d602251b21e9dfa
Matt <Matthew.sm.kim1@gmail.com>
2018-04-01 00:01:18 -0400

    deleted some comments

--- Medhacks2018/backend/appli.js
+++ Medhacks2018/backend/appli.js
@@ -3,17 +3,17 @@ form.addEventListener("submit", handleFormSubmit);
 
 
 function handleFormSubmit(event) {
-   // Stop the form from submitting since we’re handling that with AJAX.
+
    event.preventDefault();
 
    // Call our function to get the form data.
+   //data is our json object
    var data = formToJSON(form.elements);
 
    // Use `JSON.stringify()` to make the output valid, human-readable JSON.
    var output = JSON.stringify(data, null, "  ");
    alert(output);
 
-   // ...this is where we’d actually do something with the form data...
 };
 
 const formToJSON = elements => [].reduce.call(elements, (data, element) => {
@@ -21,10 +21,6 @@ const formToJSON = elements => [].reduce.call(elements, (data, element) => {
   // Make sure the element has the required properties and should be added.
   if (isValidElement(element) && isValidValue(element)) {
 
-    /*
-     * Some fields allow for more than one value, so we need to check if this
-     * is one of those fields and, if so, store the values as an array.
-     */
     if (isCheckbox(element)) {
       data[element.name] = (data[element.name] || []).concat(element.value);
     } else if (isMultiSelect(element)) {
