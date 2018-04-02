b9ba958a1333abc9781e08f265480f0a2f071a98
jorilla1 <jorilla1@jhu.edu>
2018-04-02 10:16:42 -0400

    added non=slim jquery so that we can have better animations and made modals fade instead of pop into existence

--- Medhacks2018/js/main.js
+++ Medhacks2018/js/main.js
@@ -2,26 +2,26 @@ $(document).ready(() => {
     let $curwindow;
 
     $('#track1').on('click', () => {
-        $('.black-overlay').toggle();
-        $('#track1-info').toggle();
+        $('.black-overlay').fadeToggle('fast');
+        $('#track1-info').fadeToggle('medium');
         $curwindow = $('#track1-info');
     });
 
     $('#track2').on('click', () => {
-        $('.black-overlay').toggle('medium');
-        $('#track2-info').toggle('slow');
+        $('.black-overlay').fadeToggle('fast');
+        $('#track2-info').fadeToggle('medium');
         $curwindow = $('#track2-info');
     });
 
     $('#track3').on('click', () => {
-        $('.black-overlay').toggle('medium');
-        $('#track3-info').toggle('slow');
+        $('.black-overlay').fadeToggle('fast');
+        $('#track3-info').fadeToggle('medium');
         $curwindow = $('#track3-info');
     });
 
     $('.black-overlay').on('click', () => {
-        $('.black-overlay').toggle();
-        $curwindow.toggle();
+        $('.black-overlay').fadeToggle('fast');
+        $curwindow.fadeToggle('fast');
     });
 
 });
